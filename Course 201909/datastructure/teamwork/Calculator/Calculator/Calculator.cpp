de0c8b09282f949b0a898a9c6dc0525f1371d075
SLRYZD <suozhengduo@hotmail.com>
2019-11-25 16:43:16 +0800

    Update

---
+++ Course 201909/datastructure/teamwork/Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,180 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <stack>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+double cal_ans(string infix);
+
+const char list_priority[8][8] = { //运算符优先级表
+	//        +      -      *      /      ^      (      )      \0
+	/*+*/    '>',   '>',   '<',   '<',   '<',   '<',   '>',   '>',
+	/*-*/    '>',   '>',   '<',   '<',   '<',   '<',   '>',   '>',
+	/***/    '>',   '>',   '>',   '>',   '<',   '<',   '>',   '>',
+	/*/*/    '>',   '>',   '>',   '>',   '<',   '<',   '>',   '>',
+	/*^*/    '>',   '>',   '>',   '>',   '>',   '<',   '>',   '>',
+	/*(*/    '<',   '<',   '<',   '<',   '<',   '<',   '=',   ' ',
+	/*)*/    ' ',   ' ',   ' ',   ' ',   ' ',   ' ',   ' ',   ' ',
+	/*\0*/   '<',   '<',   '<',   '<',   '<',   '<',   ' ',   '=' };
+
+void err()
+{
+	cout << "error" << endl;
+	exit(-1);
+}
+
+double read_digit(char*& ptr)
+{
+	double ans;
+	sscanf_s(ptr, "%lf", &ans); //在所给指针指向位置读入一个双浮点数 储存在ans中
+
+	while (isdigit(*ptr) || *ptr == '.') ++ptr; //移动指针至该数字之后
+
+	return ans;
+}
+
+int num_symbol(char a)
+{
+	switch (a) //将运算符转为数字 方便通过优先级表判断优先级
+	{
+		case '+': return 0;
+		case '-': return 1;
+		case '*': return 2;
+		case '/': return 3;
+		case '^': return 4;
+		case '(': return 5;
+		case ')': return 6;
+		case '\0': return 7;
+		default: err(); //未定义的运算符
+	}
+}
+
+char cmp_priority(char stack, char now) //比较栈顶与当前运算符优先级大小
+{
+	unsigned int _stack = num_symbol(stack), _now = num_symbol(now);
+	return list_priority[_stack][_now];
+}
+
+double cal_binary(double d1, char opt, double d2) //返回两数字在给定运算后的值
+{
+	switch (opt)
+	{
+		case '+': return d1 + d2;
+		case '-': return d1 - d2;
+		case '*': return d1 * d2;
+		case '/': return d1 / d2;
+		case '^': return pow(d1, d2);
+		default: err();
+	}
+}
+
+void cal_una(string& s, unsigned int sp) //unary 替换一元函数为数字
+{
+	char* ptr = &s[0] + sp;
+	unsigned int ed = sp;
+	while (*ptr != '(') {++ptr; ++ed;} //指向一元函数左括号
+	++ptr; ++ed;
+	unsigned int cnt = 1, st = ed;
+	while (cnt != 0)
+	{
+		if (*ptr == '(') ++cnt;
+		else if (*ptr == ')') --cnt;
+		++ptr; ++ed;
+	}
+
+	stringstream ss_substr;
+	string ans_substr;
+
+	if (ed - st - 1 == 0) err(); //输入为空 判断输入异常
+
+	if (s.find("sin", sp) != string::npos) ss_substr << sin(cal_ans(s.substr(st, ed - st - 1)));
+	else if (s.find("cos", sp) != string::npos) ss_substr << cos(cal_ans(s.substr(st, ed - st - 1)));
+	else if (s.find("ln", sp) != string::npos) ss_substr << log(cal_ans(s.substr(st, ed - st - 1)));
+	else if (s.find("log", sp) != string::npos) ss_substr << log10(cal_ans(s.substr(st, ed - st - 1)));
+	else err();
+
+	ss_substr >> ans_substr; //数字转为字符串
+	if (ans_substr == "") err(); //计算结果为空 判断输入异常
+	s.replace(sp, ed - sp, ans_substr);
+
+	return;
+}
+
+void cal_bin(string& s, unsigned int sp) //binary 二元函数
+{
+	return;
+}
+
+double cal_ans(string infix) //将表达式字符串替换为数字
+{
+	stack<double> digit;
+	stack<char> symbol;
+	symbol.push('\0');
+
+	unsigned int special;
+	while ((special = infix.find("sin(")) != string::npos) cal_una(infix,special);
+	while ((special = infix.find("cos(")) != string::npos) cal_una(infix, special);
+	while ((special = infix.find("ln(")) != string::npos) cal_una(infix, special);
+	while ((special = infix.find("log(")) != string::npos) cal_una(infix, special); //log()理解为lg()
+
+	char* ptr = &infix[0];
+	while (!symbol.empty())
+	{
+		if (isdigit(*ptr)) digit.push(read_digit(ptr));
+		else
+		{
+			switch (cmp_priority(symbol.top(), *ptr))
+			{
+			case '<': {
+				symbol.push(*(ptr++)); //将更优先运算符压入 并向后移动指针
+				break;
+			}
+			case '>': { //栈顶运算符优先度更高 可实行之前的计算
+				char operat = symbol.top(); symbol.pop();
+				double digit1 = digit.top(); digit.pop();
+				double digit2 = digit.top(); digit.pop();
+				digit.push(cal_binary(digit2, operat, digit1));
+				break;
+			}
+			case '=': { //当且仅当右括号遇到左括号 将左括号弹出即可
+				symbol.pop();
+				++ptr;
+				break;
+			}
+			default: err(); //未定义的优先级
+			}
+		}
+	}
+
+	return digit.top(); //返回计算完毕后剩下的数字 即该表达式字符串的结果
+}
+
+void erase_space(string& s) //忽略空格 增强鲁棒性
+{
+	unsigned int pos = 0;
+	while ((pos = s.find(" ", pos)) != string::npos) s.erase(pos, 1);
+
+	return;
+}
+
+void formatize(string& s) //将表达式格式化为可处理的形式
+{
+	erase_space(s);
+	s.erase(s.find("="), 1);
+
+	return;
+}
+
+int main()
+{
+	string infix;
+	getline(cin, infix);
+	formatize(infix);
+
+	cout << setprecision(4) << fixed << cal_ans(infix); //输出保留小数点后四位
+
+	return 0;
+}
---
+++ Course 201909/datastructure/teamwork/MinimumSpanningTree/MinimumSpanningTree/MinimumSpanningTree.cpp
@@ -0,0 +1,293 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+#define maxvtxnum 10
+#define INF 999
+typedef int adjmatrix[maxvtxnum][maxvtxnum];
+typedef struct
+{
+	adjmatrix arcs;
+	int vtxnum, arcnum;
+}graph;
+
+typedef struct
+{
+	bool visited;
+	int distance;
+	int from;
+}buf_Prim;
+
+typedef struct
+{
+	int a;
+	int b;
+	int distance;
+}buf_arc_Kruskal;
+
+graph g;
+
+void Prim_init_buf(graph& g, buf_Prim* buf)
+{
+	g.arcnum = 0;
+	for (int i = 0; i < g.vtxnum; ++i)
+	{
+		buf[i].visited = false;
+		buf[i].distance = INF;
+		buf[i].from = 0;
+		for (int j = 0; j < g.vtxnum; ++j) g.arcs[i][j] = INF;
+	}
+	return;
+}
+
+void Prim_update(graph g, buf_Prim* buf, int which)
+{
+	for (int i = 0; i < g.vtxnum; ++i)
+	{
+		if (!buf[i].visited && g.arcs[which][i] < buf[i].distance)
+		{
+			buf[i].distance = g.arcs[which][i];
+			buf[i].from = which;
+		}
+	}
+	return;
+}
+
+void Prim_create_path(graph& g, buf_Prim* buf, int which)
+{
+	++g.arcnum;
+	g.arcs[which][buf[which].from] = g.arcs[buf[which].from][which] = buf[which].distance;
+	return;
+}
+
+int Prim_pick_shortest(graph g, buf_Prim* buf)
+{
+	int which = 0,nowmax = INF;
+	for (int i = 0; i < g.vtxnum; ++i)
+	{
+		if (!buf[i].visited && buf[i].distance < nowmax)
+		{
+			which = i;
+			nowmax = buf[i].distance;
+		}
+	}
+	return which;
+}
+
+graph algorithm_Prim(graph g)
+{
+	graph span = g;
+	buf_Prim* vtx = new buf_Prim[g.vtxnum];
+	Prim_init_buf(span, vtx);
+
+	int cnt = 0;
+	vtx[0].visited = true;
+	int i = 0;
+	while (cnt != g.vtxnum)
+	{
+		Prim_update(g, vtx, i);
+		i = Prim_pick_shortest(g, vtx);
+		Prim_create_path(span, vtx, i);
+		vtx[i].visited = true;
+		++cnt;
+	}
+	
+	delete [] vtx;
+	return span;
+}
+
+int Kruskal_ancestor(int which, int* ancest)
+{
+	if (ancest[which] == which) return which;
+	return Kruskal_ancestor(ancest[which], ancest);
+}
+
+void Kruskal_merge(int a, int b,int* ancest)
+{
+	int ancest_a = Kruskal_ancestor(a, ancest),
+		ancest_b = Kruskal_ancestor(b, ancest);
+
+	if (ancest_a != ancest_b) ancest[ancest_b] = ancest_a;
+	return;
+}
+
+void Kruskal_get_arc(graph g, buf_arc_Kruskal* buf)
+{
+	int k = 0;
+	for (int i = 0; i < g.vtxnum; ++i)
+		for (int j = i + 1; j < g.vtxnum; ++j)
+			if (g.arcs[i][j] != INF)
+			{
+				buf[k].a = i;
+				buf[k].b = j;
+				buf[k].distance = g.arcs[i][j];
+				++k;
+			}
+	return;
+}
+
+bool cmp_buf_arc_Kruskal(buf_arc_Kruskal a, buf_arc_Kruskal b)
+{
+	return a.distance < b.distance;
+}
+
+void Kruskal_init_buf(graph& g, buf_arc_Kruskal* buf, int* ancest)
+{
+	g.arcnum = 0;
+	for (int i = 0; i < g.vtxnum; ++i)
+	{
+		buf[i].a = 0;
+		buf[i].b = 0;
+		buf[i].distance = INF;
+		ancest[i] = i;
+		for (int j = 0; j < g.vtxnum; ++j) g.arcs[i][j] = INF;
+	}
+	return;
+}
+
+bool Kruskal_connected(graph g, int* ancest)
+{
+	bool flag = false;
+	for (int i = 0; i < g.vtxnum; ++i)
+	{
+		if (ancest[i] == i)
+		{
+			if (flag) return false;
+			flag = true;
+		}
+	}
+	return true;
+}
+
+graph algorithm_Kruskal(graph g)
+{
+	graph span = g;
+	buf_arc_Kruskal* arc = new buf_arc_Kruskal[g.arcnum];
+	int* ancest = new int[g.vtxnum];
+	Kruskal_init_buf(span, arc, ancest);
+
+	Kruskal_get_arc(g, arc);
+	sort(arc,arc+g.arcnum, cmp_buf_arc_Kruskal);
+
+	for (int i = 0; i < g.arcnum; ++i)
+	{
+		if (Kruskal_ancestor(arc[i].a, ancest) != Kruskal_ancestor(arc[i].b, ancest))
+		{
+			Kruskal_merge(arc[i].a, arc[i].b, ancest);
+			span.arcs[arc[i].a][arc[i].b] = arc[i].distance;
+			span.arcs[arc[i].b][arc[i].a] = arc[i].distance;
+			++span.arcnum;
+			if (Kruskal_connected(span, ancest)) break;
+		}
+	}
+
+	delete [] arc;
+	delete [] ancest;
+	return span;
+}
+
+bool cmp_buf_arc_Removingarc(buf_arc_Kruskal a, buf_arc_Kruskal b)
+{
+	return a.distance > b.distance;
+}
+
+bool Removingarc_connected(graph g, bool* visited)
+{
+	queue<int> que;
+	que.push(0);
+	visited[0] = true;
+	while (!que.empty())
+	{
+		for (int i = 0; i < g.vtxnum; ++i)
+		{
+			if (!visited[i] && g.arcs[que.front()][i] != INF)
+			{
+				visited[i] = true;
+				que.push(i);
+			}
+		}
+		que.pop();
+	}
+
+	for (int i = 0; i < g.vtxnum; ++i)
+		if (visited[i] == false) return false;
+
+	return true;
+}
+
+graph algorithm_Removingarc(graph g)
+{
+	buf_arc_Kruskal* arc = new buf_arc_Kruskal[g.arcnum];
+	bool* visited = new bool[g.vtxnum];
+	Kruskal_get_arc(g, arc);
+	sort(arc, arc + g.arcnum, cmp_buf_arc_Removingarc);
+
+	graph g_buf = g;
+	int k = 0;
+	do
+	{
+		for (int i = 0; i < g.vtxnum; ++i) visited[i] = false;
+		g = g_buf;
+		g_buf.arcs[arc[k].a][arc[k].b] = INF;
+		g_buf.arcs[arc[k].b][arc[k].a] = INF;
+		++k;
+	} while (Removingarc_connected(g_buf,visited));
+
+	delete [] arc;
+	delete [] visited;
+	return g;
+}
+
+void input_matrix(graph& g)
+{
+	cout << "输入无向图的邻接矩阵(若无边则输入999)：" << endl;
+	for (int i = 0; i < g.vtxnum; ++i)
+		for (int j = 0; j < g.vtxnum; ++j) cin >> g.arcs[i][j];
+	return;
+}
+
+void output_matrix(graph g)
+{
+	for (int i = 0; i < g.vtxnum; ++i)
+	{
+		for (int j = 0; j < g.vtxnum; ++j) cout << g.arcs[i][j] << " ";
+		cout << endl;
+	}
+	return;
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	cout << "输入无向图结点数目：";
+	cin >> g.vtxnum; 
+	cout << "输入无向图边数目：";
+	cin >> g.arcnum;
+	input_matrix(g);
+
+	int choice;
+	
+	cout << "1. Prim" << endl;
+	cout << "2. Kruskal" << endl;
+	cout << "3. Removing-arc" << endl;
+	cout << "选择算法："; cin >> choice;
+	switch (choice)
+	{
+	case 1: {
+		output_matrix(algorithm_Prim(g));
+		break;
+	}
+	case 2: {
+		output_matrix(algorithm_Kruskal(g));
+		break;
+	}
+	case 3: {
+		output_matrix(algorithm_Removingarc(g));
+		break;
+	}
+	default: exit(-1);
+	}
+
+	return 0;
+}
